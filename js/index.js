c556bab6829cba20f7cb58272960a7ed7c3c0f47
Jeriah Bowers <jeriah97bowers@yahoo.com>
2021-01-14 20:43:09 -0500

    working on mobile navigation functionality

--- js/index.js
+++ js/index.js
@@ -1,19 +1,20 @@
 //mobile drop-down nav functionality
 const dropDownNavController = (function() {
-    const _dropDown = document.querySelector('.navigation__wrapper');
+    const _wrapper = document.querySelector('.navigation__wrapper');
+    const _dropDown = document.querySelector('.drop-down');
     const _navButton = document.querySelector('.mobile-menu');
     const _navList = document.querySelector('.navigation__list');
     const _contactButton = document.querySelector('.navigation .contact-button');
 
     _navButton.addEventListener('click', () => {
-        if(_dropDown.style.display === 'none') {
-            _dropDown.style.display = "flex";
-            _navList.style.display = "flex";
-            _contactButton.style.display = "inline-block";
+        if(window.getComputedStyle(_navList).visibility === "hidden") {
+            _dropDown.style.transform = 'scale(70)';
+            _navList.style.visibility = 'visible';
+            _contactButton.style.visibility = 'visible';
         } else {
-            _dropDown.style.display = "none";
-            _navList.style.display = "none";
-            _contactButton.style.display = "none";
+            _dropDown.style.transform = 'scale(1)';
+            _navList.style.visibility = 'hidden';
+            _contactButton.style.visibility = 'hidden';
         }
     });
 }());
