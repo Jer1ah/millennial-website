50b69e70758745b2e4133bc5e5fc81a3f9e09a82
Jeriah Bowers <jeriah97bowers@yahoo.com>
2021-04-04 11:41:17 -0400

    making blog and newsletter section responsive

--- js/index.js
+++ js/index.js
@@ -1,20 +1,21 @@
 //mobile drop-down nav functionality
-const dropDownNavController = (function() {
-    const _wrapper = document.querySelector('.navigation__wrapper');
-    const _dropDown = document.querySelector('.drop-down');
-    const _navButton = document.querySelector('.mobile-menu');
-    const _navList = document.querySelector('.navigation__list');
-    const _contactButton = document.querySelector('.navigation .contact-button');
 
-    _navButton.addEventListener('click', () => {
-        if(window.getComputedStyle(_navList).visibility === "hidden") {
-            _dropDown.style.transform = 'scale(70)';
-            _navList.style.visibility = 'visible';
-            _contactButton.style.visibility = 'visible';
-        } else {
-            _dropDown.style.transform = 'scale(1)';
-            _navList.style.visibility = 'hidden';
-            _contactButton.style.visibility = 'hidden';
-        }
-    });
-}());
+// const dropDownNavController = (function() {
+//     const _wrapper = document.querySelector('.navigation__wrapper');
+//     const _dropDown = document.querySelector('.drop-down');
+//     const _navButton = document.querySelector('.mobile-menu');
+//     const _navList = document.querySelector('.navigation__list');
+//     const _contactButton = document.querySelector('.navigation .contact-button');
+
+//     _navButton.addEventListener('click', () => {
+//         if(window.getComputedStyle(_navList).visibility === "hidden") {
+//             _dropDown.style.transform = 'scale(70)';
+//             _navList.style.visibility = 'visible';
+//             _contactButton.style.visibility = 'visible';
+//         } else {
+//             _dropDown.style.transform = 'scale(1)';
+//             _navList.style.visibility = 'hidden';
+//             _contactButton.style.visibility = 'hidden';
+//         }
+//     });
+// }());
